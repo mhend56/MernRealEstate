468127a2b95a128deb7fbe2e4c841a74f7d7a48f
mhend56 <mhend56@gmail.com>
2023-10-05 13:46:41 +1300

    create pages and routes

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,23 @@
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+
+import About from './pages/About';
+import Home from './pages/Home';
+import SignIn from './pages/SignIn';
+import SignUp from './pages/SignUp';
+import Profile from './pages/Profile';
+
 const App = () => {
-  return <h1 className="text-red-500">Testing</h1>;
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/sign-in" element={<SignIn />} />
+        <Route path="/sign-up" element={<SignUp />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/profile" element={<Profile />} />
+      </Routes>
+    </BrowserRouter>
+  );
 };
 
 export default App;
