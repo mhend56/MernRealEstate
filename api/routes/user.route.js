a0206bd389bcef5db4747509596e26d4608ce777
mhend56 <mhend56@gmail.com>
2023-10-09 09:06:58 +1300

    creat test api with controller

--- api/routes/user.route.js
+++ api/routes/user.route.js
@@ -1,10 +1,7 @@
 import express from 'express';
+import { test } from '../controllers/user.controller.js';
 const router = express.Router();
-router.get('/test', (req, res) => {
-  res.json({
-    message: 'Hello World!',
-  });
-});
+router.get('/test', test);
 export default router;
 // app.get('/test', (req, res) => {
 //   res.send('Hello world');
