24895da45feed3c4433479af282d393fddc0fc89
mhend56 <mhend56@gmail.com>
2023-10-22 12:22:54 +1300

    Complete update user functionality

--- api/utils/verifyUser.js
+++ api/utils/verifyUser.js
@@ -1,9 +1,9 @@
 import jwt from 'jsonwebtoken'
-
+import {errorHandler} from './error.js'
 
 export const verifyToken =(req,res,next)=>{
     const token = req.cookies.access_token
-    if(!token) return next(errorHandlere(402,'Unauthorized'))
+    if(!token) return next(errorHandler(402,'Unauthorized'))
     jwt.verify(token,process.env.JWT_SECRET,(err,user)=>{
         if(err) return next(errorHandler(403,'Forbidden'))
         req.user=user
