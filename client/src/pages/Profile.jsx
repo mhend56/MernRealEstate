91eadb99e8633400b8df7379146de014f00d3525
mhend56 <mhend56@gmail.com>
2023-10-24 11:12:34 +1300

    User sign out

--- client/src/pages/Profile.jsx
+++ client/src/pages/Profile.jsx
@@ -3,7 +3,7 @@ import { useSelector,useDispatch } from 'react-redux';
 import { useState, useRef,useEffect } from 'react';
 import {getStorage,ref,uploadBytesResumable,getDownloadURL} from 'firebase/storage'
 import {app} from '../firebase'
-import { updateUserStart,updateUserSuccess,updateUserFailure,deleteUserStart,deleteUserSuccess,deleteUserFailure } from '../redux/user/userSlice';
+import { updateUserStart,updateUserSuccess,updateUserFailure,deleteUserStart,deleteUserSuccess,deleteUserFailure,signOutUserStart,signOutUserSuccess,signOutUserFailure } from '../redux/user/userSlice';
 const Profile = () => {
   const fileRef = useRef(null);
   const { currentUser,loading,error } = useSelector((state) => state.user);
@@ -89,6 +89,20 @@ const handleDeleteUser = async()=>{
 
 }
 
+const handleSignOut = async()=>{
+    try {
+        dispatch(signOutUserStart())
+        const res =await fetch('/api/auth/signout')
+        const data =await res.json()
+        if(data.success===false){
+      dispatch(signOutUserFailure(data.message))
+        return}
+        dispatch(signOutUserSuccess(data))
+    } catch (error) {
+        dispatch(signOutUserFailure(error.message))
+    }
+}
+
   return (
     <div className="p-3 max-w-lg mx-auto">
       <h1 className="text-3xl font-semibold text-center my-7">Profile</h1>
@@ -136,7 +150,7 @@ const handleDeleteUser = async()=>{
       </form>
       <div className="flex justify-between mt-5">
         <span onClick={handleDeleteUser} className="text-red-700 cursor-pointer">Delete Account</span>
-        <span className="text-red-700 cursor-pointer">Sign Out</span>
+        <span onClick={handleSignOut} className="text-red-700 cursor-pointer">Sign Out</span>
       </div>
       <p className='text-red-700 mt-5'>{error ? error:''}</p>
       <p className =' text-green-700 mt-5'>{updateSuccess ? 'User is updated successfully':''}</p>
