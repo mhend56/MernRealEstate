2acf523b4c9f2c8219a5dca25e854bab4a18f325
mhend56 <mhend56@gmail.com>
2023-10-28 10:06:16 +1300

    Create listing api route

--- api/index.js
+++ api/index.js
@@ -5,6 +5,7 @@ dotenv.config();
 
 import userRouter from './routes/user.route.js';
 import authRouter from './routes/auth.route.js';
+import listingRouter from './routes/listing.route.js'
 import cookieParser from 'cookie-parser'
 mongoose
   .connect(process.env.MONGO)
@@ -23,6 +24,8 @@ app.listen(3000, () => {
 });
 app.use('/api/user', userRouter);
 app.use('/api/auth', authRouter);
+app.use('/api/listing', listingRouter);
+
 app.use((err, req, res, next) => {
   const statusCode = err.statusCode || 500;
   const message = err.message || 'Internal Server Error';
