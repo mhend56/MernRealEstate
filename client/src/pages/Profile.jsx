03bcb126c359774a0e9504c12112959293e8593f
mhend56 <mhend56@gmail.com>
2023-11-06 13:40:36 +1300

    Complete show user listings functionality

--- client/src/pages/Profile.jsx
+++ client/src/pages/Profile.jsx
@@ -13,6 +13,9 @@ const Profile = () => {
   const [fileUploadError,setFileUploadError]=useState(false)
 const [formData,setFormData]=useState({})
 const [updateSuccess,setUpdateSuccess]=useState(false)
+const [showListingError,setShowListingError] =useState(false)
+const [userListings,setUserListings] =useState([])
+
 const dispatch = useDispatch()
 
 
@@ -104,6 +107,26 @@ const handleSignOut = async()=>{
     }
 }
 
+const handleShowListings= async()=>{
+    try {
+        setShowListingError(false)
+        const res = await fetch(`api/user/listings/${currentUser._id}`)
+        const data=await res.json()
+        if(data.success===false){
+            setShowListingError(true)
+            return
+        }
+        setUserListings(data)
+      
+     
+        
+    } catch (error) {
+        setShowListingError(true)
+        
+    }
+}
+
+
   return (
     <div className="p-3 max-w-lg mx-auto">
       <h1 className="text-3xl font-semibold text-center my-7">Profile</h1>
@@ -145,7 +168,7 @@ const handleSignOut = async()=>{
           className="border p-3 rounded-lg"
           onChange={handleChange}
         />
-        <button disabled = {loading} className="bg-slate-700 text-white rounded-lg p-3 uppercase hover:opac{ity-95 disabled:opacity-80">
+        <button disabled = {loading} className="bg-slate-700 text-white rounded-lg p-3 uppercase hover:opacity-95 disabled:opacity-80">
           {loading ? 'Loading...':'Update'}
         </button>
         <Link  className= 'bg-green-700 text-white p-3 rounded-lg uppercase text-center hover:opacity-95'to={"/create-listing"}>Create Listing</Link>
@@ -156,6 +179,31 @@ const handleSignOut = async()=>{
       </div>
       <p className='text-red-700 mt-5'>{error ? error:''}</p>
       <p className =' text-green-700 mt-5'>{updateSuccess ? 'User is updated successfully':''}</p>
+      <button onClick ={handleShowListings} className= 'text-green-700 w-full'>Show Listings</button>
+      <p className= 'text-red-700 mt-5'>{showListingError ? 'Error show Listing':''}</p>
+    
+      {userListings && userListings.length > 0 && 
+      <div  className="flex flex-col gap-4">
+        <h1 className= 'text-center mt-7 text-2xl fonr-semibold'>Your Listings</h1>
+      {userListings.map((listing)=> (
+        <div key = {listing._id} className= 'border rounded-lg p-3 flex justify-between items-center gap-4'>
+            <Link to={`/listing/${listing._id}`}>
+                <img src = {listing.imageUrls[0]} alt='listing cover'className='h-16 w-16 object-contain'/>
+            </Link>
+            <Link    className='text-slate-700 font-semibold hover:underline truncate flex-1'
+            to={`/listing/${listing._id}`}>
+                <p>{listing.name}</p>
+            </Link>
+      <div className='flex flex-col gap-4 item-center'>
+        <button className='text-red-700 uppercase'>delete</button>
+        <button className='text-green-700 uppercase'>edit</button>
+      </div>
+
+        </div>
+      ))}
+
+      </div>}
+      
     </div>
   );
 };
