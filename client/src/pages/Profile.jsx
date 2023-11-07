61dc25eae35c6e17575987ff45667ba141498131
mhend56 <mhend56@gmail.com>
2023-11-08 10:13:56 +1300

    Delete user listing

--- client/src/pages/Profile.jsx
+++ client/src/pages/Profile.jsx
@@ -126,7 +126,22 @@ const handleShowListings= async()=>{
     }
 }
 
+const handleListingDelete = async(listingId)=>{
+    try {
+        const res = await fetch (`/api/listing/delete/${listingId}`,{
+            method:'DELETE'
+        })
+        const data= await res.json()
+        if(data.success===false){
+            console.log(data.message)
+            return
+        }
+        setUserListings((prev)=>prev.filter((listing)=>listing._id !== listingId))
+    } catch (error) {
+        console.log(error.message)
+    }
 
+}
   return (
     <div className="p-3 max-w-lg mx-auto">
       <h1 className="text-3xl font-semibold text-center my-7">Profile</h1>
@@ -195,7 +210,7 @@ const handleShowListings= async()=>{
                 <p>{listing.name}</p>
             </Link>
       <div className='flex flex-col gap-4 item-center'>
-        <button className='text-red-700 uppercase'>delete</button>
+        <button onClick = {()=>handleListingDelete(listing._id)} className='text-red-700 uppercase'>delete</button>
         <button className='text-green-700 uppercase'>edit</button>
       </div>
 
