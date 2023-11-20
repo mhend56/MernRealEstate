af7c6a1b29089879d81bfbd34a42641c3128d6c3
mhend56 <mhend56@gmail.com>
2023-11-21 09:39:45 +1300

    About page details added

--- client/src/pages/About.jsx
+++ client/src/pages/About.jsx
@@ -1,7 +1,11 @@
 import React from 'react';
 
 const About = () => {
-  return <div>About</div>;
+  return <div className='py-20 px-4 max-w-6xl mx-auto'><h1 className ='text-3xl font-bold mb-4 text-slate-800'>About Country Trails</h1>
+  <p className='mb-4 text-slate-700'> sit amet consectetur, adipisicing elit. Officiis id fugit recusandae consequatur ab minus corrupti nobis perspiciatis repellat quis inventore blanditiis, suscipit dignissimos modi omnis, deserunt culpa sit nesciunt quaerat saepe quo. Autem ipsam delectus fuga explicabo, ratione doloribus in vitae accusantium culpa incidunt possimus ducimus deleniti ea qui.</p>
+  <p className='mb-4 text-slate-700'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptatum reprehenderit, numquam voluptate dolorum ipsum omnis deserunt corporis? Obcaecati modi cumque, facilis laudantium reprehenderit natus. Laudantium corrupti suscipit aperiam nostrum, ipsam pariatur sit corporis, quos dignissimos, nulla dicta temporibus beatae vel repellat sed assumenda. Fugit necessitatibus consectetur esse assumenda quis! Natus.</p>
+  <p className='mb-4 text-slate-700'>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Fugit porro, doloremque quae fuga odio exercitationem impedit dolorum ullam reiciendis, unde aut quia corrupti! Nostrum, rem. Dolorem repellat, ea aliquam totam, in dolorum sed similique quidem obcaecati, odio necessitatibus assumenda veniam provident commodi quae eveniet? Nisi, doloremque explicabo. Quae, ea sint?</p>
+  </div>
 };
 
 export default About;
